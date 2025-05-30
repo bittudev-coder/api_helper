eb71e6b2d3833699b71afda065d878c1e68393dc
BITTU KUMAR <bittu50520@gmail.com>
2025-05-30 22:10:48 +0530

    Fix package file names and update tracked files to match api_handler

--- test/api_handler_test.dart
+++ test/api_handler_test.dart
@@ -1,16 +1,14 @@
-import 'package:api_helper/api_helper.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('A group of tests', () {
-    final awesome = Awesome();
+
 
     setUp(() {
       // Additional setup goes here.
     });
 
     test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
     });
   });
 }
